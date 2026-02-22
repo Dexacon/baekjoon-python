eed13910c2a4971dab1447abc5477857f7965ba9
ChanHee Lee <136292082+Dexacon@users.noreply.github.com>
2026-02-22 18:32:50 +0900

    [Silver III] Title: 소수 구하기, Time: 328 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -8,6 +8,12 @@ def DecimalCheck(a):
     return True
 input = sys.stdin.readline
 M,N = map(int,input().split())
+result = [True] * (N+1)
+result[0] = result[1] = False
+for i in range(2,int(N**0.5)+1):
+    if result[i]:
+        for j in range(i*i,N+1,i):
+            result[j] = False
 for i in range(M,N+1):
-    if DecimalCheck(i):
+    if result[i]:
         print(i)
