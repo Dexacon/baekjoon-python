8e740d35585b695e2b6111aef1ae85a7ea3bd34f
ChanHee Lee <136292082+Dexacon@users.noreply.github.com>
2026-02-22 15:02:23 +0900

    [Silver IV] Title: 다음 소수, Time: 1648 ms, Memory: 36008 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,16 @@
+import sys,functools
+def DecimalCheck(a):
+    if n < 2:
+        return False
+    for i in range(2,int(a**(0.5))+1):
+        if a % i == 0:
+            return False
+    return True
+T = int(sys.stdin.readline())
+for i in range(T):
+    n = int(sys.stdin.readline())
+    while True:
+        if DecimalCheck(n):
+            break
+        n += 1
+    print(n)
