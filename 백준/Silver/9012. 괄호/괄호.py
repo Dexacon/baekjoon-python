a617d8af6384b227ea5ad043557310982ccde2ef
ChanHee Lee <136292082+Dexacon@users.noreply.github.com>
2026-02-25 17:33:00 +0900

    [Silver IV] Title: 괄호, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for i in range(T):
+    n =input().rstrip()
+    cnt = 0
+    for i in n:
+        if i == '(':
+            cnt += 1
+        elif i == ')':
+            cnt -= 1
+        if cnt < 0:
+            print('NO')
+            break
+    if cnt > 0 :
+        print('NO')
+    elif cnt == 0:
+        print('YES')
+        
