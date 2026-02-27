7f5389354364da22e98344026d3f2f6afd95f45d
ChanHee Lee <136292082+Dexacon@users.noreply.github.com>
2026-02-27 14:58:42 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 N = int(input())
-card = deque([i for i in range(1,N+1)])
+card = deque(range(1,N+1))
 while len(card) > 1:
     card.popleft()
     card.append(card.popleft())
