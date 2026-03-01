525a5f4afc87a775368739c7622b8a2cbe000375
ChanHee Lee <136292082+Dexacon@users.noreply.github.com>
2026-03-01 17:05:56 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 296 ms, Memory: 54892 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 N,M = map(int,input().split())
-result = dict()
+dictionary = dict()
 for _ in range(N):
     X = input().rstrip()
     if len(X) >= M:
-        if X in result:
-            result[X] += 1
+        if X in dictionary:
+            dictionary[X] += 1
         else:
-            result[X] = 1
-dictionary = sorted(result.keys(),key=lambda x:(-result[x],-len(x),x))
-for i in dictionary:
+            dictionary[X] = 1
+result = sorted(dictionary.keys(),key=lambda x:(-dictionary[x],-len(x),x))
+for i in result:
     print(i)
