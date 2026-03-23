f030984443a995544e5549dba39de8214545db47
ChanHee Lee <136292082+Dexacon@users.noreply.github.com>
2026-03-23 23:57:00 +0900

    [Silver III] Title: 프린터 큐, Time: 64 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    priorities = list(map(int, input().split()))
+    queue = deque((p, i) for i, p in enumerate(priorities))
+    count = 0
+    while queue:
+        current = queue.popleft()
+        if any(current[0] < q[0] for q in queue):
+            queue.append(current)
+        else:
+            count += 1
+            if current[1] == M:
+                print(count)
+                break
